b1c6964c130662eda689c7dd191e1c1822283618
Kevin Tran <kevin.tran@leonardo.com>
2019-06-26 14:42:07 -0400

    Load list & array configuration from yaml file

---
+++ pro01-on-condition/src/main/java/org/tnmk/practicespringconfiguration/pro01oncondition/Pro01OnConditionApplication.java
@@ -0,0 +1,15 @@
+package org.tnmk.practicespringconfiguration.pro01oncondition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+//@EnableConfigurationProperties
+@SpringBootApplication
+public class Pro01OnConditionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Pro01OnConditionApplication.class, args);
+  }
+}
