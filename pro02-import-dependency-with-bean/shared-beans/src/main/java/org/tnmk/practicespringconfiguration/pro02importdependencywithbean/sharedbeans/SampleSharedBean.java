7f0c794ce504f97569f277b94723a0a666848b4a
trank <Kevin.Tran@pointclickcare.com>
2022-12-22 09:37:56 -0500

    Update pro02: test different values in application.properties in submodule vs mainmoduel.

--- pro02-import-dependency-with-bean/shared-beans/src/main/java/org/tnmk/practicespringconfiguration/pro02importdependencywithbean/sharedbeans/SampleSharedBean.java
+++ pro02-import-dependency-with-bean/shared-beans/src/main/java/org/tnmk/practicespringconfiguration/pro02importdependencywithbean/sharedbeans/SampleSharedBean.java
@@ -2,6 +2,7 @@ package org.tnmk.practicespringconfiguration.pro02importdependencywithbean.share
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -10,14 +11,17 @@ import java.lang.invoke.MethodHandles;
 
 @Component
 public class SampleSharedBean {
-    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  private static final Logger logger =
+      LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  @Value("${sample-config.value}")
+  private String sampleConfigValue;
 
-    public SampleSharedBean(){
-        logger.info("Initiate SampleSharedBean "+this);
-    }
+  public SampleSharedBean() {
+    logger.info("Constructor SampleSharedBean {}", this, sampleConfigValue);
+  }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void start(){
-        logger.info("Start SampleSharedBean "+this);
-    }
+  @EventListener(ApplicationReadyEvent.class)
+  public void start() {
+    logger.info("Start SampleSharedBean {}", this, sampleConfigValue);
+  }
 }
