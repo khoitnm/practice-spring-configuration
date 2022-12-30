c1a67b1477642abe8b133bf2f3c9b54c043be301
Khoi Tran <khoi.tnm@gmail.com>
2022-12-30 11:03:53 -0500

    Update comment in SimpleBeanConfig

--- pro00-simple-config/src/main/java/org/tnmk/practicespringconfiguration/pro00simpleconfig/config/SimpleBeanConfig.java
+++ pro00-simple-config/src/main/java/org/tnmk/practicespringconfiguration/pro00simpleconfig/config/SimpleBeanConfig.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Config {
+public class SimpleBeanConfig {
+  // NOTE: prefix must be "bean-a", cannot use "beanA"
   @Bean("beanA")
   @ConfigurationProperties(prefix = "bean-a")
   public SimpleBean beanA() {
